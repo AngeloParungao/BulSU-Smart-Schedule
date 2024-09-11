1227f6a63f46dc8141761ef907caaf250600f73b
AngeloParungao <angeloparungao.ap@gmail.com>
2024-09-11 23:35:52 +0800

    refactor: remove logs

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -26,17 +26,13 @@ router.post('/login', (req, res) => {
                     console.error('Error comparing passwords:', err);
                     return res.status(500).json({ error: 'Failed to compare passwords' });
                 }
-                
-                // Log the hashed password and plain text password for debugging purposes
-                console.log("Password provided:", password);
-                console.log("Hashed password stored in DB:", user.password);
-                
                 if (isMatch) {
                     console.log("Login Successful:", user); // Log successful login
                     return res.status(200).json({
                         message: 'Login successful',
                         user_id: user.user_id,
-                        department_code: user.department_code
+                        department_code: user.department_code,
+                        role: user.role
                     });
                 } else {
                     console.log("Login Failed:", user); // Log failed login
