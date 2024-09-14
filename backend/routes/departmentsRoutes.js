6f80340a4bdac24b661ea72c6c18735f5827bf12
AngeloParungao <angeloparungao.ap@gmail.com>
2024-09-14 11:18:29 +0800

    first commit

---
+++ backend/routes/departmentsRoutes.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const db = require('../database');
+const router = express.Router();
+
+router.get('/fetch', (req, res) => {
+    let sql = "SELECT * FROM departments";
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            console.error('Error fetching departments:', err);
+            return res.status(500).json({ error: 'Failed to fetch departments' });
+        }
+        res.status(200).json(results);
+    });
+});
+
+module.exports = router;
