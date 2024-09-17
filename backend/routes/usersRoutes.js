d943b720f68430fd659cd209c1a0e6d108a27f10
AngeloParungao <angeloparungao.ap@gmail.com>
2024-09-17 21:11:55 +0800

    feat: email new user

--- backend/routes/usersRoutes.js
+++ backend/routes/usersRoutes.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const db = require('../config/database');
 const router = express.Router();
+const bcrypt = require('bcrypt');
+require('dotenv').config();
+const { sendEmail } = require('../services/emailer');
 
-router.post('/adding', (req, res) => {
-    const { email, first_name, middle_name, last_name, department_code } = req.body;
+router.post('/adding', async (req, res) => {
+    const { email, first_name, middle_name, last_name, department_code, password } = req.body;
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const sql = "INSERT INTO users (email, first_name, middle_name, last_name, department_code, password) VALUES (?, ?, ?, ?, ?, ?)";
 
-    const sql = "INSERT INTO users (email, first_name, middle_name, last_name, department_code) VALUES (?, ?, ?, ?, ?)";
+    try {
+        const result = await new Promise((resolve, reject) => {
+            db.query(sql, [email, first_name, middle_name, last_name, department_code, hashedPassword], (err, result) => {
+                if (err) {
+                    return reject(err);
+                }
+                resolve(result);
+            });
+        });
 
-    db.query(sql, [email, first_name, middle_name, last_name, department_code], (err, result) => {
-        if (err) {
-            console.error('Error inserting data:', err);
-            return res.status(500).json({ error: 'Failed to add user' });
-        }
-        res.status(200).json({ message: 'User added successfully' });
-    });
+        const subject = "Welcome to Our Platform";
+        const html = `<p>Hello ${last_name}, ${first_name} ${middle_name}</p><p>Your profile has been created successfully.</p><p>Credentials for Logging in to your account are as follows:</p><p>Email: ${email}</p><p>Password: ${password}</p>`;
+
+        await sendEmail(email, subject, html);
+
+        res.status(200).json({ message: 'User added successfully and email sent.' });
+    } catch (error) {
+        console.error("Error adding user or sending email:", error);
+        res.status(500).json({ error: 'Failed to add user or send email' });
+    }
 });
+
 
 router.get('/fetch', (req, res) => {
     const sql = "SELECT * FROM users";
