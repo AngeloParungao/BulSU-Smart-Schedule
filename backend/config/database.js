0e2ec9f170289dc3c032b25f16e856574b95993c
AngeloParungao <angeloparungao.ap@gmail.com>
2024-09-18 23:32:37 +0800

    chore: mysql2

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql');
+const mysql = require('mysql2');
 require('dotenv').config();
 
 const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
