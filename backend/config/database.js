b6f77491a8573be9c6243e90fd14e4af931a34f6
AngeloParungao <angeloparungao.ap@gmail.com>
2024-09-18 22:26:17 +0800

    refactor: change connection

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,9 +1,14 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+//const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
 
-const db = mysql.createConnection(urlDB);
+const db = mysql.createConnection({
+   host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
 
 /*host: process.env.DB_HOST,
     user: process.env.DB_USER,
