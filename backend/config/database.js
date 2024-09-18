4f8798af5ddfeef9427705c51251b3fc79cb0986
AngeloParungao <angeloparungao.ap@gmail.com>
2024-09-18 21:04:01 +0800

    feat: for deployment

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,12 +1,14 @@
 const mysql = require('mysql');
 require('dotenv').config();
 
-const db = mysql.createConnection({
-    host: process.env.DB_HOST,
+const urlDB = `mysql://${process.env.DB_USER}:${process.env.DB_PASSWORD}@${process.env.DB_HOST}:${process.env.DB_PORT}/${process.env.DB_NAME}`
+
+const db = mysql.createConnection(urlDB);
+
+/*host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-});
+    database: process.env.DB_NAME*/
 
 db.connect(err => {
     if (err) {
