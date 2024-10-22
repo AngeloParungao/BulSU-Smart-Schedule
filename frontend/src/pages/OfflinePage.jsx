7f752642d589b3f4037b6d0520e911f154b9d61e
AngeloParungao <angeloparungao.ap@gmail.com>
2024-10-22 22:47:53 +0800

    first commit

---
+++ frontend/src/pages/OfflinePage.jsx
@@ -0,0 +1,10 @@
+// OfflinePage.js
+import React from "react";
+
+const OfflinePage = () => (
+  <div className="h-screen flex flex-col items-center justify-center bg-gray-200">
+    <h1 className="text-6xl font-bold">No Internet Connection</h1>
+    <p className="text-2xl mt-4">Please check your connection and try again.</p>
+  </div>
+);
+export default OfflinePage;
