b5d5fd1a48b6d5955c1eadf4dc29543845c017c2
AngeloParungao <angeloparungao.ap@gmail.com>
2024-11-03 14:59:58 +0800

    feat: join program and department code

--- backend/routes/departmentsRoutes.js
+++ backend/routes/departmentsRoutes.js
@@ -3,11 +3,11 @@ const db = require('../config/database');
 const router = express.Router();
 
 router.post('/adding', (req, res) => {
-    const { department, department_code, department_head } = req.body;
+    const { department, department_code, program_code, department_head } = req.body;
 
     const sql = "INSERT INTO departments (department, department_code, department_head) VALUES (?, ?, ?)";
 
-    db.query(sql, [department, department_code, department_head], (err, result) => {
+    db.query(sql, [department, program_code + " (" + department_code + ")", department_head], (err, result) => {
         if (err) {
             console.error('Error inserting data:', err);
             return res.status(500).json({ error: 'Failed to add department' });
@@ -49,11 +49,11 @@ router.delete('/delete', (req, res) => {
 
 router.put('/update/:code', (req, res) => {
     const deptCode = req.params.code;
-    const { department, department_code, department_head } = req.body;
+    const { department, department_code, program_code, department_head } = req.body;
 
     const sql = "UPDATE departments SET department = ?, department_code = ?, department_head = ? WHERE department_code = ?";
 
-    db.query(sql, [department, department_code, department_head, deptCode], (err, result) => {
+    db.query(sql, [department, program_code + " (" + department_code + ")", department_head, deptCode], (err, result) => {
         if (err) {
             console.error('Error updating department:', err);
             return res.status(500).json({ error: 'Failed to update department' });
