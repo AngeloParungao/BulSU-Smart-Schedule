6691b6dc3bbe88ae1e97b7f65b9e7637273acde3
AngeloParungao <angeloparungao.ap@gmail.com>
2024-11-11 17:04:21 +0800

    feat: fix filtering for department

--- frontend/src/pages/Instructors.jsx
+++ frontend/src/pages/Instructors.jsx
@@ -32,7 +32,7 @@ const Instructors = () => {
     last_name: "",
     work_type: "",
     tags: "",
-    department_code: currentRole === "Administrator" ? "" : currentDepartment,
+    department_code: "",
     old_full_name: "",
   });
 
@@ -71,7 +71,7 @@ const Instructors = () => {
       first_name: "",
       middle_name: "",
       last_name: "",
-      department_code: currentRole === "Administrator" ? "" : currentDepartment,
+      department_code: "",
       work_type: "",
       tags: "",
       old_full_name: "",
@@ -429,40 +429,44 @@ const Instructors = () => {
                   <option value="Part-timer">Part Time</option>
                 </select>
               </div>
-              {currentRole === "Administrator" && (
-                <div className="flex flex-col gap-[0.2rem] w-[100%]">
-                  <div className="flex items-center gap-2">
-                    <label htmlFor="department" className="text-sm text-black">
-                      Department:
-                    </label>
-                    {errors.department_code && (
-                      <p className="text-red-500 text-xs">
-                        {errors.department_code}
-                      </p>
-                    )}
-                  </div>
-                  <select
-                    name="department"
-                    id="department"
-                    value={data.department_code}
-                    onChange={(e) => {
-                      setData({ ...data, department_code: e.target.value });
-                      setErrors({ ...errors, department_code: "" });
-                    }}
-                    className={`${
-                      errors.department_code ? "border-red-500" : ""
-                    } p-[0.5rem] text-black text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500}`}
-                  >
-                    <option value="">Department</option>
-                    <option value="GENERAL">GENERAL</option>
-                    {departments.map((department, index) => (
-                      <option key={index} value={department.code}>
-                        {department.department_code}
+              <div className="flex flex-col gap-[0.2rem] w-[100%]">
+                <div className="flex items-center gap-2">
+                  <label htmlFor="department" className="text-sm text-black">
+                    Department:
+                  </label>
+                  {errors.department_code && (
+                    <p className="text-red-500 text-xs">
+                      {errors.department_code}
+                    </p>
+                  )}
+                </div>
+                <select
+                  name="department"
+                  id="department"
+                  value={data.department_code}
+                  onChange={(e) => {
+                    setData({ ...data, department_code: e.target.value });
+                    setErrors({ ...errors, department_code: "" });
+                  }}
+                  className={`${
+                    errors.department_code ? "border-red-500" : ""
+                  } p-[0.5rem] text-black text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500}`}
+                >
+                  <option value="">Department</option>
+                  <option value="GENERAL">GENERAL</option>
+                  {departments
+                    .filter(
+                      (d) =>
+                        currentRole === "Administrator" ||
+                        d.department_code === currentDepartment
+                    )
+                    .map((d, index) => (
+                      <option key={index} value={d.department_code}>
+                        {d.department_code}
                       </option>
                     ))}
-                  </select>
-                </div>
-              )}
+                </select>
+              </div>
             </div>
             <div className="flex flex-col gap-[0.2rem]">
               <label htmlFor="tags" className="text-sm text-black">
@@ -528,29 +532,31 @@ const Instructors = () => {
                       <option value="Part-timer">Part Time</option>
                     </select>
                   </div>
-                  {currentRole === "Administrator" && (
-                    <div className="flex items-center gap-2">
-                      <label
-                        htmlFor="department"
-                        className="text-sm text-black"
-                      >
-                        Department:
-                      </label>
-                      <select
-                        className="p-[0.2rem] text-black text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
-                        id="department"
-                        value={selectedDepartment}
-                        onChange={(e) => setSelectedDepartment(e.target.value)}
-                      >
-                        <option value="All">All</option>
-                        {departments.map((department, index) => (
-                          <option key={index} value={department.code}>
-                            {department.department_code}
+                  <div className="flex items-center gap-2">
+                    <label htmlFor="department" className="text-sm text-black">
+                      Department:
+                    </label>
+                    <select
+                      className="p-[0.2rem] text-black text-sm border border-gray-300 rounded-md focus:outline-none focus:ring-1 focus:ring-blue-500"
+                      id="department"
+                      value={selectedDepartment}
+                      onChange={(e) => setSelectedDepartment(e.target.value)}
+                    >
+                      <option value="All">All</option>
+                      <option value="GENERAL">GENERAL</option>
+                      {departments
+                        .filter(
+                          (d) =>
+                            currentRole === "Administrator" ||
+                            d.department_code === currentDepartment
+                        )
+                        .map((d, index) => (
+                          <option key={index} value={d.department_code}>
+                            {d.department_code}
                           </option>
                         ))}
-                      </select>
-                    </div>
-                  )}
+                    </select>
+                  </div>
                 </div>
               </div>
               <div className="flex gap-4">
@@ -597,14 +603,10 @@ const Instructors = () => {
                 <thead>
                   <tr className="border-b border-gray-300 bg-gray-100">
                     <th className="md:w-10 w-6"></th>
-
-                    {currentRole === "Administrator" && (
-                      <th className="text-xs md:text-[1rem] py-2">
-                        Department
-                      </th>
-                    )}
+                    <th className="text-xs md:text-[1rem] py-2">Department</th>
                     <th className="text-xs md:text-[1rem] py-2">Instructor</th>
                     <th className="text-xs md:text-[1rem] py-2">Email</th>
+                    <th className="text-xs md:text-[1rem] py-2">Work</th>
                     <th className="text-xs md:text-[1rem] py-2">Labels</th>
                     <th className="md:w-10 w-6"></th>
                   </tr>
@@ -623,11 +625,9 @@ const Instructors = () => {
                           }
                         />
                       </td>
-                      {currentRole === "Administrator" && (
-                        <td className="md:p-2 md:text-sm text-[0.6rem] border-r border-gray-300">
-                          {instructor.department_code}
-                        </td>
-                      )}
+                      <td className="md:p-2 md:text-sm text-[0.6rem] border-r border-gray-300">
+                        {instructor.department_code}
+                      </td>
                       <td className="md:p-2 md:text-sm text-[0.6rem] border-r border-gray-300">{`${instructor.first_name} ${instructor.middle_name} ${instructor.last_name}`}</td>
                       <td
                         className="p-2 md:text-sm text-[0.6rem] border-r border-gray-300"
@@ -637,6 +637,9 @@ const Instructors = () => {
                         }}
                       >
                         {instructor.email}
+                      </td>
+                      <td className="md:p-2 md:text-sm text-[0.6rem] border-r border-gray-300">
+                        {instructor.work_type}
                       </td>
                       <td className="md:p-2 md:text-sm text-[0.6rem] border-r border-gray-300">
                         {instructor.tags}
